cabcefeb7df063ab91f7f1bfad8cdb1a8eb2e59d
shivam17acelucid <shivam17101@gmail.com>
2022-09-06 11:05:27 +0530

    otp api fix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mongoSanitize = require("express-mongo-sanitize");
 const app = express();
 const mongoose = require("mongoose");
-var bodyparser = require("body-parser");
+const bodyparser = require("body-parser");
 
-var cors = require('cors')
+const cors = require('cors')
 
 require("dotenv").config();
 const Route = require("./routes/index");
